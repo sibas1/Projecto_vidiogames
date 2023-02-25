6b5d5d7dfe0f56fd7b0ed21a8bffff4fc9281d4e
sibas1 <117487764+sibas1@users.noreply.github.com>
2023-02-25 15:55:56 -0500

    1
    1

--- api/src/db.js
+++ api/src/db.js
@@ -53,6 +53,7 @@ const popgenres = async () => {
 }     
 popgenres();
 
+
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
